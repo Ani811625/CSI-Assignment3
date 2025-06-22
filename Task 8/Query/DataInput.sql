e4b4bc3f71a8cee54650dcaebe6bb42d5a8b0422
Aniruddha Sarkar <145282516+Ani811625@users.noreply.github.com>
2025-06-22 20:33:00 +0530

    Create DataInput.sql

---
+++ Task 8/Query/DataInput.sql
@@ -0,0 +1,13 @@
+INSERT INTO OCCUPATIONS (Name, Occupation) VALUES
+('Samantha', 'Doctor'),
+('Julia', 'Actor'),
+('Maria', 'Actor'),
+('Meera', 'Singer'),
+('Ashley', 'Professor'),
+('Ketty', 'Professor'),
+('Christeen', 'Professor'),
+('Jane', 'Actor'),
+('Jenny', 'Doctor'),
+('Priya', 'Singer');
+
+SELECT * FROM OCCUPATIONS;
