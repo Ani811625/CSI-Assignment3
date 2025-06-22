02621cd8d10c6d204e16fe742ec5755e39292527
Aniruddha Sarkar <145282516+Ani811625@users.noreply.github.com>
2025-06-22 19:21:32 +0530

    Create DataInput.sql

---
+++ Task 2/Query/DataInput.sql
@@ -0,0 +1,22 @@
+
+INSERT INTO Students (ID, Name) VALUES
+(1, 'Ashley'),
+(2, 'Samantha'),
+(3, 'Julia'),
+(4, 'Scarlet');
+SELECT * FROM Students;
+
+INSERT INTO Friends (ID, Friend_ID) VALUES
+(1, 2),
+(2, 3),
+(3, 4),
+(4, 1);
+SELECT * FROM Friends;
+
+INSERT INTO Packages (ID, Salary) VALUES
+(1, 15.20),
+(2, 10.06),
+(3, 11.55),
+(4, 12.12);
+SELECT * FROM Packages;
+
