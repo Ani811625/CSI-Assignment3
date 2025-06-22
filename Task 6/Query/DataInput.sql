e2e2ef1b607ce6b114782f27e83e0047887bea6d
Aniruddha Sarkar <145282516+Ani811625@users.noreply.github.com>
2025-06-22 20:27:08 +0530

    Create DataInput.sql

---
+++ Task 6/Query/DataInput.sql
@@ -0,0 +1,5 @@
+INSERT INTO STATION (ID, CITY, STATE, LAT_N, LONG_W) VALUES
+(1, 'CityA', 'State1', 24.56, 67.89),
+(2, 'CityB', 'State2', 25.00, 70.12),
+(3, 'CityC', 'State3', 26.87, 68.45),
+(4, 'CityD', 'State4', 20.15, 65.67);
