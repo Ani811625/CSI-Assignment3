191df232d58926e10ab5a3896bbed143befe2e5d
Aniruddha Sarkar <145282516+Ani811625@users.noreply.github.com>
2025-06-22 20:43:40 +0530

    Add files via upload

---
+++ Task 11/DataInput.sql
@@ -0,0 +1,22 @@
+-- Students Data
+INSERT INTO Students (ID, Name) VALUES
+(1, 'Ashley'),
+(2, 'Samantha'),
+(3, 'Julia'),
+(4, 'Scarlet');
+SELECT * FROM Students;
+-- Friends Data
+INSERT INTO Friends (ID, Friend_ID) VALUES
+(1, 2),
+(2, 3),
+(3, 4),
+(4, 1);
+SELECT * FROM Friends;
+-- Packages Data
+INSERT INTO Packages (ID, Salary) VALUES
+(1, 15.20),
+(2, 10.06),
+(3, 11.55),
+(4, 12.12);
+SELECT * FROM Packages;
+
