3a1f9a8cebd42c83d285f570872cc4fea0c2d6b6
Aniruddha Sarkar <145282516+Ani811625@users.noreply.github.com>
2025-06-22 20:16:14 +0530

    Create DataInput.sql

---
+++ Task 4/Query/DataInput.sql
@@ -0,0 +1,40 @@
+-- Contests
+INSERT INTO Contests (contest_id, hacker_id, name) VALUES
+(66406, 17973, 'Rose'),
+(66556, 79153, 'Angela'),
+(94828, 80275, 'Frank');
+
+-- Colleges
+INSERT INTO Colleges (college_id, contest_id) VALUES
+(11219, 66406),
+(32473, 66556),
+(56685, 94828);
+
+-- Challenges
+INSERT INTO Challenges (challenge_id, college_id) VALUES
+(18765, 11219),
+(47127, 11219),
+(60292, 32473),
+(72974, 56685);
+
+-- View Stats
+INSERT INTO View_Stats (challenge_id, total_views, total_unique_views) VALUES
+(47127, 26, 19),
+(47127, 15, 14),
+(18765, 43, 10),
+(18765, 72, 13),
+(75516, 35, 17),
+(60292, 11, 10),
+(72974, 41, 15),
+(75516, 75, 11);
+
+-- Submission Stats
+INSERT INTO Submission_Stats (challenge_id, total_submissions, total_accepted_submissions) VALUES
+(75516, 34, 12),
+(47127, 27, 10),
+(47127, 56, 18),
+(75516, 74, 12),
+(75516, 83, 8),
+(72974, 68, 24),
+(72974, 82, 14),
+(47127, 28, 11);
